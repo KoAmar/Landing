ec3b3cbc330fe9dfafddf524b90a8cf8fca49526
KoAmar <pa1318vel@gmail.com>
2022-05-07 16:17:11 +0300

    nav links scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+window.bootstrap = bootstrap;
 // import 'jquery';
 import 'css/main.scss';
 const $ = require('jquery')
 // require.ensure('bootstrap')
 // require.ensure('jquery')
 // jQuery.fx.off = false;
+
 function multiEvent() {
-    var navbarHeight = $('.navbar').height() + 100;
+    let navbarHeight = $('.navbar').height() + 100;
     $('.banner').css({
         "padding-top": navbarHeight,
     });
@@ -18,12 +21,27 @@ $(document).ready(function () {
     // window.scrollTo(0,0);
 
     let menuToggler = $('.menu-toggle');
+    let myCollapse = $('#navbarMenu');
+    let bsCollapse = new bootstrap.Collapse(myCollapse, {
+        toggle: false
+    })
+
     menuToggler.on('click', function () {
         $(this).toggleClass('is-active');
     });
-    // $('.navbar-nav').find('.nav-link').on('click',function(){
-    //     menuToggler.toggleClass('is-active');
-    // })
+    $('.navbar-brand').on('click', function () {
+        menuToggler.removeClass('is-active');
+        bsCollapse.hide();
+    })
+    $('.navbar-nav').find('.nav-link').on('click', function (event) {
+        event.preventDefault();
+        let blockTop = $($(this).attr("href")).offset().top;
+        let navHeight = $(".navbar").outerHeight(true) - $(".navbar-collapse").outerHeight(true);
+        window.scrollTo(0, blockTop - navHeight);
+
+        menuToggler.toggleClass('is-active');
+        bsCollapse.hide();
+    })
 
     if ($(window).width() >= 992) {
         $('.featurettes-item').on('mouseenter', function () {
