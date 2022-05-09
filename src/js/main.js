8074e4d59a096ca6b833990a50b2954a67810d07
KoAmar <pa1318vel@gmail.com>
2022-05-09 15:19:54 +0300

    before slick-carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,46 @@
 import * as bootstrap from 'bootstrap';
-
 window.bootstrap = bootstrap;
-// import 'jquery';
+// import Swiper from 'swiper';
 import 'css/main.scss';
 const $ = require('jquery')
-// require.ensure('bootstrap')
-// require.ensure('jquery')
-// jQuery.fx.off = false;
+let navHeight;
+
+function initNavHeight() {
+    // if ($(window).width() >= 992) {
+    navHeight = $(".navbar").outerHeight(true);
+    // }
+    // else {
+    //     console.log($(".navbar").outerHeight(true));
+    //     console.log($(".navbar-collapse").outerHeight(true));
+    //     navHeight = $(".navbar").outerHeight(true)// - $(".navbar-collapse").outerHeight(true);
+    // }
+}
+
+function hideLongTestimonial() {
+    if ($('.testimonials .carousel-item.active .left').height() > 350) {
+        $('.testimonials .carousel-item.active .left:not(.show-more)').addClass('show-more')
+        console.log('+++');
+
+    }
+    console.log('+');
+}
 
 function multiEvent() {
-    let navbarHeight = $('.navbar').height() + 100;
+    initNavHeight();
+
     $('.banner').css({
-        "padding-top": navbarHeight,
+        "padding-top": navHeight + 100,
     });
+
     $('#scheduleModal .modal-content').height(window.innerHeight);
+
+    $('main > *:not(.banner)').each(function () {
+        $(this).css('min-height', window.innerHeight - navHeight);
+    })
 }
 
 $(document).ready(function () {
     multiEvent();
-    // window.scrollTo(0,0);
 
     let menuToggler = $('.menu-toggle');
     let myCollapse = $('#navbarMenu');
@@ -35,22 +57,28 @@ $(document).ready(function () {
     })
     $('.navbar-nav').find('.nav-link').on('click', function (event) {
         event.preventDefault();
-        let blockTop = $($(this).attr("href")).offset().top;
-        let navHeight = $(".navbar").outerHeight(true) - $(".navbar-collapse").outerHeight(true);
-        window.scrollTo(0, blockTop - navHeight);
 
-        menuToggler.toggleClass('is-active');
-        bsCollapse.hide();
+        let href = $(this).attr("href")
+        let blockTop = $(href).offset().top;
+        history.replaceState(undefined, undefined, href)
+
+        if ($(window).width() < 992) {
+            menuToggler.toggleClass('is-active');
+            bsCollapse.hide();
+        }
+        // console.log(blockTop);
+        // console.log(navHeight);
+        window.scrollTo(0, blockTop - navHeight);
     })
 
     if ($(window).width() >= 992) {
         $('.featurettes-item').on('mouseenter', function () {
             $(this).addClass('is-active');
-            $(this).find('.featurette-text').slideDown(400)
+            $(this).find('.featurette-text').slideDown(200)
         })
         $('.featurettes-item').on('mouseleave', function () {
             $(this).removeClass('is-active');
-            $(this).find('.featurette-text').slideUp(400)
+            $(this).find('.featurette-text').slideUp(200)
         })
     }
     var scheduleModal = document.getElementById('scheduleModal')//$('#scheduleModal');
@@ -65,7 +93,24 @@ $(document).ready(function () {
         })
     })
 
+    hideLongTestimonial()
+    var testimonialsCarousel = document.getElementById('testimonialsCarousel')
+    testimonialsCarousel.addEventListener('slid.bs.carousel', function () {
+        // console.log('+');
+        // $('.testimonials .carousel-item').each(function () {
+        // $(this).css('opacity', '0')
+        // $(this).show()
+        // console.log($(this).height());
+        // if ($(this).height() > 350) {
+        //     console.log('+')
+        //     $(this).addClass('show-more')
+        // }
+        // $(this).find('*:not(active)').hide()
+        // $(this).css('opacity', '1')
+        // })
+        hideLongTestimonial()
+    })
 })
-window.addEventListener('resize', function (event) {
+window.addEventListener('resize', function () {
     multiEvent();
 });
